9727464a7030ecca4b43b0337a3e4b5e9e6c8a60
fady-albert <fadyalber2009@gmail.com>
2026-07-22 04:58:23 +0300

    add onclick function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const body = document.querySelector('body');
 const modeBtn = document.getElementById('mode');
 const modeBtnTxt = document.getElementById('modeTxt');
 const container = document.getElementById('main-con');
+const counter = document.getElementById('counter');
 
 // DEFINE JS VARIABLES
 let mode = localStorage.getItem('mode') || '';
@@ -12,6 +13,7 @@ let max = 500;
 let  color;
 let diffColor;
 let winSquare;
+let count = 0;
 
 // CHANGE MODE BETWEEN LIGHT AND DARK
 function darkMode() {
@@ -34,8 +36,11 @@ if(mode === 'dark') {
 
 // ADD HTML SQUARES
 function addSquares() {
+
+        container.innerHTML = '';
+
     for(let i = 0; i < (size * size); i++) {
-        const div = document.createElement('div');
+        const div = document.createElement('button');
         div.style.width = max/size + 'px';
         div.style.height = max/size + 'px';
         div.classList = 'square'
@@ -47,26 +52,48 @@ function addSquares() {
 function addColor() {
     const con = document.querySelectorAll('.square');
 
+    // GET RANDOM SQUARE
     winSquare = con[Math.floor(Math.random()  * (size * size))];
 
+    // CREATE MAIN COLOR
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
 
     color = `${r}, ${g}, ${b}`;
 
+    // CREATE DIFFERENT COLOR
     const dr = Math.min(r + diff, 255);
     const dg = Math.min(g + diff, 255);
     const db = Math.min(b + diff, 255);
 
     diffColor = `${dr}, ${dg}, ${db}`;
 
+    // ADD MAIN COLOR TO ALL SQUARES
     con.forEach(box => {
         box.style.background = `rgb(${color})`;
     });
 
+    // ADD DIFFERENT COLOR TO WIN SQUARE
     winSquare.style.background = `rgb(${diffColor})`;
+
+    // ONCLICK FUNCTION
+    winSquare.addEventListener('click', () => {
+        // EDIT COUNTER
+        count++
+        countTrue()
+
+        // REPLACE THE SQUARES
+        addSquares()
+        addColor()
+    })
 }
 
+// SHOW COUNTER
+function countTrue() {
+    counter.textContent = count;
+}
+
+// RUN THER MAIN FUNCTIONS
 addSquares()
 addColor()
